345e418aefdf8059702108cdeef0c775d8826ca0
Arkadiusz Bartczak <arkbar97@gmail.com>
2018-05-08 21:29:45 +0200

    Add project files.

---
+++ Saper/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Saper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SaperEngine engine;
+        List<Button> ButtonList;
+
+        public MainWindow()
+        {
+            engine = new SaperEngine();
+            InitializeComponent();
+            createBoard(15, 13, 14);
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            int x = 0, y = 0;
+            var button = (Button)sender;
+
+            GetCord(button.Name, out x, out y);
+            if (engine.BombField[y][x] == 0)
+            {               
+                OpenFields(x , y);
+            }
+            else
+                button.Content = engine.BombField[y][x];        
+        }
+
+        private void OpenFields(int x, int y)
+        {
+          
+            int xz = engine.BombField[0].Count();
+            int yz = engine.BombField.Count();
+
+            if (x < 0 || x >= engine.BombField[0].Count()) return;
+            if (y < 0 || y >= engine.BombField.Count()) return;
+
+            var button = ButtonList.SingleOrDefault(r => r.Name == "I" + y + "I" + x);
+            if (button.Content == null) return;
+
+            if (engine.BombField[y][x] == 0)
+                button.Content = null;
+
+            if (engine.BombField[y][x] != 9 && engine.BombField[y][x] != 0)
+                button.Content = engine.BombField[y][x];
+
+            if (engine.BombField[y][x] != 0) return;
+
+            OpenFields(x - 1, y - 1);
+            OpenFields(x - 1, y);
+            OpenFields(x - 1, y + 1);
+            OpenFields(x + 1, y - 1);
+            OpenFields(x + 1, y);
+            OpenFields(x + 1, y + 1);
+            OpenFields(x, y - 1);
+            OpenFields(x, y);
+            OpenFields(x, y + 1);
+        }
+
+        private void GetCord(string name, out int x, out int y)
+        {
+            string[] Cord = name.Split('I');
+            y = Convert.ToInt32(Cord[1]);
+            x = Convert.ToInt32(Cord[2]);
+        }
+
+        private void createBoard(int x,int y, int Bombs)
+        {
+            Button pole;
+            ButtonList = new List<Button>();
+            Board.RowDefinitions.Clear();
+            Board.ColumnDefinitions.Clear();
+            Board.Children.Clear();
+
+            for (int j = 0; j < x; j++)
+                Board.ColumnDefinitions.Add(new ColumnDefinition());
+
+            for (int i = 0; i < y; i++)
+                Board.RowDefinitions.Add(new RowDefinition());
+
+            for (int i = 0; i < y; i++)
+                for (int j = 0; j < x; j++)
+                {
+                    pole = new Button();
+                    pole.Background = Brushes.Gray;
+                    pole.Name ="I" + i + "I" + j;
+                    pole.SetValue(Grid.RowProperty, i);
+                    pole.SetValue(Grid.ColumnProperty, j);
+                    pole.Click += new RoutedEventHandler(OnClick);
+                    pole.Content = pole.Name;
+                    ButtonList.Add(pole);
+                    Board.Children.Add(pole);
+                    
+                }
+            engine.SetBoard(x, y, Bombs);
+        }
+    }
+}
---
+++ Saper/SaperEngine.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saper
+{
+    class SaperEngine
+    {
+        public List<List<int>> BombField { get; set; }
+        public int boardX, boardY;
+        Random rnd;
+
+
+        public int CountBombs(int fieldY,int fieldX)
+        {
+            if (BombField[fieldY][fieldX] == 9)
+                return 9;
+
+            int count = 0;
+
+
+            for (int j = -1; j < 2; j++)
+                for (int i = -1; i < 2; i++)
+                {
+                    if ((fieldY + j) < 0 || (fieldX + i) < 0) continue;
+                    if ((fieldY + j) >= BombField.Count() || (fieldX + i) >= BombField[0].Count()) continue;
+
+                    if (BombField[fieldY + j][fieldX + i] == 9)
+                        count++;
+                }
+
+            return count;
+        }
+
+        private void setAllFields()
+        {
+            for (int j = 0; j < BombField.Count(); j++)
+                for (int i = 0; i < BombField[0].Count(); i++)
+                    BombField[j][i] = CountBombs(j, i);
+        }
+
+        private void CreateBombs(int Bombs)
+        {
+            int x;
+            int y;
+
+            while (0 < Bombs)
+            {
+                y = rnd.Next(0, BombField.Count());
+                x = rnd.Next(0, BombField[0].Count());
+
+                if (BombField[y][x] != 9)
+                {
+                    BombField[y][x] = 9;
+                    Bombs--;
+                }
+            }
+        }
+
+        public void SetBoard(int bX, int bY,int Bombs)
+        {
+            rnd = new Random();
+
+            boardX = bX;
+            boardY = bY;
+
+            BombField = new List<List<int>>();
+
+            for (int i = 0; i < boardY; i++)
+            {
+                BombField.Add(new List<int>());
+                for (int j = 0; j < boardX; j++)
+                    BombField[i].Add(0);
+            }
+            CreateBombs(Bombs);
+            setAllFields();
+        }      
+
+    }
+}
